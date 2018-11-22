940a4e762feb0e6178489e905c48caebcd57fb18
TheAutoTrimp <45256441+TheAutoTrimp@users.noreply.github.com>
2018-11-22 11:16:52 +0100

    Update MasteryCheck.js

--- MasteryCheck.js
+++ MasteryCheck.js
@@ -1,7 +1,11 @@
 setInterval(() => {
-  let enoughWind = getNextNatureCost('Wind') < game.empowerments.Wind.tokens + game.empowerments.Ice.tokens * 0.8;
+  let convertRate = 0.1 * game.talents.nature.purchased ? (game.talents.nature2.purchased ? 8 : 6) : 5;
+  
+  let enoughWind = getNextNatureCost('Wind') < game.empowerments.Wind.tokens + game.empowerments.Ice.tokens * convertRate;
   let enoughPoison = getNextNatureCost('Poison') < game.empowerments.Poison.tokens;
+
   let alertVisible = document.getElementById('masteryAlert');
+  
   if (!alertVisible && (enoughWind || enoughPoison)) {
     let alert = '<span id="masteryAlert" class="alert badge">!</span>';
     document.getElementById('natureA').insertAdjacentHTML('afterbegin', alert);
